83da9ea153ac0a9e997ab45cd53559b8ccb5493d
Sahareior Sijan <sahareiors@gmail.com>
2025-12-28 12:07:27 +0600

    homepage completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,44 @@
-import { useState } from 'react'
+
 import Hero from './components/homepage/Hero'
 import GradeLevel from './components/homepage/GradeLevel'
-import Carousel from './components/homepage/Carousel'
+import Carousel from './components/homepage/Carousel/Carousel'
+import AboutCard from './components/homepage/AboutCard'
+import AchivementCard from './components/homepage/AchivementCard'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
 <div>
 <Hero />
-<GradeLevel />
+
+<div className=''
+    style={{
+        background: 'linear-gradient(180deg, #EBF6FF 0%, #F5FAFF 100%)'
+
+    }}
+>
+  <GradeLevel />
+  <h2 className='text-[30px] px-24 font-semibold'>Populer Stories</h2>
 <Carousel />
 
+<div className='flex flex-col gap-3 justify-center items-center'>
+  <button
+  className='px-7 py-2 text-white rounded-3xl'
+  style={{
+    background: 'linear-gradient(270deg, #98D8C8 -0.1%, #1F3A2B 100.1%)'
+  }}
+  >
+    Start Reading Now!
+  </button>
+  <p className='text-[#6A7282]'>
+    Sign up to unlock all stories
+  </p>
+</div>
+</div>
+<AboutCard />
+<AchivementCard />
 </div>
   )
 }
